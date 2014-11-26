b868e31caab9c92d0e45cc932b8d386f13aa326c
Nikolay Mishin <mi@ya.ru>
2014-11-26 09:57:49 +0300

    Create set_env_perl_5_15.bat

---
+++ set_env_perl_5_15.bat
@@ -0,0 +1,9 @@
+set PATH=C:\Program Files\IBM\SQLLIB\BIN\..\db2tss\bin;C:\PROGRA~1\IBM\SQLLIB\BIN\..\db2tss\bin;C:\Program Files\IBM\SQLLIB\BIN;C:\Program Files\IBM\SQLLIB\FUNCTION;C:\IBM\InformationServer\ASBNode\apps\jre\bin\classic;C:\IBM\InformationServer\ASBNode\lib\cpp;C:\IBM\InformationServer\ASBNode\apps\proxy\cpp\vc60\MT_dll\bin;C:\windows\system32;C:\windows;C:\windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0\;C:\Program Files (x86)\IBM\Client Access\Emulator;C:\Program Files (x86)\IBM\Client Access\Shared;C:\Program Files (x86)\IBM\Client Access\;"c:\Program Files\IBM\SQLLIB\include" ;C:\Program Files\nodejs;C:\Program Files (x86)\Microsoft SQL Server\100\Tools\Binn\;C:\Program Files (x86)\Microsoft SQL Server\100\DTS\Binn\;C:\Users\rb102870\AppData\Roaming\npm;C:\Dwimperl\perl\bin;C:\Dwimperl\perl\site\bin;C:\Dwimperl\c\bin;"c:\Program Files (x86)\GnuWin32\bin" 
+
+set MKS_TK_PATH_BACK=
+set NUTCROOT=
+set ROOTDIR=
+set SHELL=
+set TERM=
+set TERMCAP=
+set TERMINFO=
